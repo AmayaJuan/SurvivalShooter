488271d9001870349fda1d0cb7af4024ac669a32
AmayaJuan <jpva360@gmail.com>
2017-08-03 00:20:41 -0500

    Tutorial 11
    Generando Enemigos

--- SurvivalShooterTutorial/Assets/SurvivalShooter/Scripts/Managers/EnemyManager.cs
+++ SurvivalShooterTutorial/Assets/SurvivalShooter/Scripts/Managers/EnemyManager.cs
@@ -7,22 +7,16 @@ public class EnemyManager : MonoBehaviour
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
-
     void Start ()
     {
         InvokeRepeating ("Spawn", spawnTime, spawnTime);
     }
 
-
     void Spawn ()
     {
         if(playerHealth.currentHealth <= 0f)
-        {
             return;
-        }
-
         int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-
         Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
     }
 }
