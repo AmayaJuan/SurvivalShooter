84a4989c53b5fad4997e56977a7e2d27a7073f01
AmayaJuan <jpva360@gmail.com>
2017-08-02 20:49:00 -0500

    Tutorial 07
    Recibiendo da;o

--- SurvivalShooterTutorial/Assets/SurvivalShooter/Scripts/Enemy/EnemyAttack.cs
+++ SurvivalShooterTutorial/Assets/SurvivalShooter/Scripts/Enemy/EnemyAttack.cs
@@ -3,17 +3,15 @@ using System.Collections;
 
 public class EnemyAttack : MonoBehaviour
 {
-    public float timeBetweenAttacks = 0.5f;
+    public float timeBetweenAttacks = .5f;
     public int attackDamage = 10;
 
-
+    bool playerInRange;
+    float timer;
     Animator anim;
     GameObject player;
     PlayerHealth playerHealth;
     //EnemyHealth enemyHealth;
-    bool playerInRange;
-    float timer;
-
 
     void Awake ()
     {
@@ -23,48 +21,31 @@ public class EnemyAttack : MonoBehaviour
         anim = GetComponent <Animator> ();
     }
 
-
-    void OnTriggerEnter (Collider other)
-    {
-        if(other.gameObject == player)
-        {
-            playerInRange = true;
-        }
-    }
-
-
-    void OnTriggerExit (Collider other)
-    {
-        if(other.gameObject == player)
-        {
-            playerInRange = false;
-        }
-    }
-
-
     void Update ()
     {
         timer += Time.deltaTime;
-
         if(timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
-        {
             Attack ();
-        }
-
         if(playerHealth.currentHealth <= 0)
-        {
             anim.SetTrigger ("PlayerDead");
-        }
     }
-
 
     void Attack ()
     {
         timer = 0f;
-
         if(playerHealth.currentHealth > 0)
-        {
             playerHealth.TakeDamage (attackDamage);
-        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+            playerInRange = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+            playerInRange = false;
     }
 }
--- SurvivalShooterTutorial/Assets/SurvivalShooter/Scripts/Player/PlayerHealth.cs
+++ SurvivalShooterTutorial/Assets/SurvivalShooter/Scripts/Player/PlayerHealth.cs
@@ -14,67 +14,49 @@ public class PlayerHealth : MonoBehaviour
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
-
+    bool isDead;
+    bool damaged;
     Animator anim;
     AudioSource playerAudio;
     PlayerMovement playerMovement;
     //PlayerShooting playerShooting;
-    bool isDead;
-    bool damaged;
-
-
+   
     void Awake ()
     {
-        anim = GetComponent <Animator> ();
-        playerAudio = GetComponent <AudioSource> ();
-        playerMovement = GetComponent <PlayerMovement> ();
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerMovement>();
         //playerShooting = GetComponentInChildren <PlayerShooting> ();
         currentHealth = startingHealth;
     }
 
-
     void Update ()
     {
         if(damaged)
-        {
             damageImage.color = flashColour;
-        }
         else
-        {
             damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
         damaged = false;
     }
-
 
     public void TakeDamage (int amount)
     {
         damaged = true;
-
         currentHealth -= amount;
-
         healthSlider.value = currentHealth;
-
-        playerAudio.Play ();
-
-        if(currentHealth <= 0 && !isDead)
-        {
-            Death ();
-        }
+        playerAudio.Play();
+        if (currentHealth <= 0 && !isDead)
+            Death();
     }
 
 
     void Death ()
     {
         isDead = true;
-
         //playerShooting.DisableEffects ();
-
-        anim.SetTrigger ("Die");
-
+        anim.SetTrigger("Die");
         playerAudio.clip = deathClip;
-        playerAudio.Play ();
-
+        playerAudio.Play();
         playerMovement.enabled = false;
         //playerShooting.enabled = false;
     }
@@ -82,6 +64,6 @@ public class PlayerHealth : MonoBehaviour
 
     public void RestartLevel ()
     {
-        SceneManager.LoadScene (0);
+        SceneManager.LoadScene(0);
     }
 }
