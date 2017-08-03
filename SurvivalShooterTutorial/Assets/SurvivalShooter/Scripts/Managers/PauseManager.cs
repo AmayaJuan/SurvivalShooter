1e1d972148fe45e1a413a88fb27eb2363ca9faa7
AmayaJuan <jpva360@gmail.com>
2017-08-03 16:09:26 -0500

    Tutorial 15
    Pantalla de pausa 2 de 2

--- SurvivalShooterTutorial/Assets/SurvivalShooter/Scripts/Managers/PauseManager.cs
+++ SurvivalShooterTutorial/Assets/SurvivalShooter/Scripts/Managers/PauseManager.cs
@@ -5,14 +5,29 @@ using UnityEngine.UI;
 using UnityEditor;
 #endif
 
-public class PauseManager : MonoBehaviour {
-    
+public class PauseManager : MonoBehaviour
+{
+    public Slider musicVolumeSlider;
+    public Slider fXVolumeSlider;
     Canvas canvas;
     
+    void LoadState()
+    {
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        fXVolumeSlider.value = PlayerPrefs.GetFloat("FXVolume", 1f);
+    }
+
+    void SaveState()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", musicVolumeSlider.value);
+        PlayerPrefs.SetFloat("FXVolume", fXVolumeSlider.value);
+    }
+
     void Start()
     {
         canvas = GetComponent<Canvas>();
         canvas.enabled = false;
+        LoadState();
     }
     
     void Update()
@@ -25,10 +40,13 @@ public class PauseManager : MonoBehaviour {
     {
         canvas.enabled = !canvas.enabled;
         Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        if (!canvas.enabled)
+            SaveState();
     }
     
     public void Quit()
     {
+        SaveState();
         #if UNITY_EDITOR 
         EditorApplication.isPlaying = false;
         #else 
